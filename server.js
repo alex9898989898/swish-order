d8641ee3f3c6730c13f18064bc18abca1ce48332
alex9898989898 <alex.lool.98.98@gmail.com>
2025-08-14 11:40:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const server = app.listen(port, () => {
 const wss = new WebSocket.Server({ noServer: true });
 let screenClients = [];
 
+// WebSocket-anslutning
 wss.on("connection", (ws) => {
   screenClients.push(ws);
   ws.on("close", () => {
@@ -23,21 +24,25 @@ wss.on("connection", (ws) => {
   });
 });
 
+// Hantera upgrade för WebSocket
 server.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
     wss.emit("connection", ws, request);
   });
 });
 
+// Hantera nya orders
 app.post("/order", (req, res) => {
   const { amount, message } = req.body;
   const orderNumber = Math.floor(Math.random() * 100000);
 
   console.log(`Ny beställning: ${orderNumber} - ${amount} kr - ${message}`);
 
+  // Skicka hela objektet till alla anslutna clients
+  const orderData = { orderNumber, amount, message };
   screenClients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(orderNumber.toString());
+      client.send(JSON.stringify(orderData));
     }
   });
 
